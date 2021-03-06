5f7ed37b7e5cef41c75d6323f08334c2bec43677
Lamanris <sprite_kg@mail.ru>
2021-03-06 13:14:08 +0400

    asdf

--- script.js
+++ script.js
@@ -5,18 +5,40 @@ let filterImg = document.querySelector('.filter-img')
 let filterInput = document.querySelector('#filterInput')
 let inputNum = document.querySelector('.input-num')
 
-
 changeFilter('blur')
 
+let currentEffect = 'blur'
+filterInput.addEventListener('input', () => {
+    if (currentEffect === 'blur') blurEffect()
+    if (currentEffect === 'sepia') sepiaEffect()
+    if (currentEffect === 'invert') invertEffect()
+    inputNum.textContent = `${filterInput.value}%`
+})
 blur.addEventListener('click', () => {
+    // blurEffect()
+    // currentEffect ='blur'
     changeFilter('blur')
 })
 sepia.addEventListener('click', () => {
+    // sepiaEffect()
+    // currentEffect ='sepia'
     changeFilter('sepia')
 })
 invert.addEventListener('click', () => {
+    // invertEffect()
+    // currentEffect ='invert'
     changeFilter('invert')
 })
+// function blurEffect () {
+//     filterImg.style.filter = `blur(${filterInput.value}px)`
+// }
+// function sepiaEffect () {
+//     filterImg.style.filter = `sepia(${filterInput.value}%)`
+// }
+// function invertEffect () {
+//     filterImg.style.filter = `invert(${filterInput.value}%)`
+// }
+
 function changeFilter (filterType) {
     filterInput.value = 0
     filterImg.style.filter = 'none'
@@ -24,8 +46,8 @@ function changeFilter (filterType) {
     filterInput.addEventListener('input', () => {
         if (filterType === 'blur') {
             filterImg.style.filter = `${filterType}(${filterInput.value}px)`
-            inputNum.textContent = `${filterInput.value}%`
         }
+        inputNum.textContent = `${filterInput.value}%`
         filterImg.style.filter = `${filterType}(${filterInput.value}%)`
     })
 }
