9b860b6a8d694ec078110f5a99751625c1c78597
Lamanris <sprite_kg@mail.ru>
2021-03-06 12:13:15 +0400

    fd

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let sepia = document.querySelector('.sepia')
 let invert = document.querySelector('.invert')
 let filterImg = document.querySelector('.filter-img')
 let filterInput = document.querySelector('#filterInput')
+let inputNum = document.querySelector('.input-num')
+
+
+changeFilter('blur')
 
 blur.addEventListener('click', () => {
     changeFilter('blur')
@@ -19,10 +23,13 @@ function changeFilter (filterType) {
     filterInput.addEventListener('input', () => {
         if (filterType === 'blur') {
             filterImg.style.filter = `${filterType}(${filterInput.value}px)`
+            inputNum.textContent = `${filterInput.value}%`
         }
         filterImg.style.filter = `${filterType}(${filterInput.value}%)`
     })
 }
+
+
 
 
 
